c5a60592b5b4ece6b7f459e838d672737d18c68c
ZTX <948881549@qq.com>
2018-04-02 14:05:15 +0800

    增加语种切换 优化界面布局

--- app/src/main/java/com/bnuz/ztx/translateapp/Ui/SettingActivity.java
+++ app/src/main/java/com/bnuz/ztx/translateapp/Ui/SettingActivity.java
@@ -5,18 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.bnuz.ztx.translateapp.R;
 
+import org.angmarch.views.NiceSpinner;
+
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by ZTX on 2018/4/2.
  */
 
 public class SettingActivity extends AppCompatActivity {
+    NiceSpinner niceSpinner;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_activity);
-        init();
+        initView();
     }
 
-    private void init() {
+    private void initView() {
+        niceSpinner = (NiceSpinner) findViewById(R.id.exchangLanguage_np);
+        List<String> data = new LinkedList<>();
+        data.add(getResources().getString(R.string.Exchange_language_Chinese_item));
+        data.add(getResources().getString(R.string.Exchange_language_English_item));
+        niceSpinner.attachDataSource(data);
     }
 }
