d6fdb9aff6b8f520843c3bac04b4374fa5594177
ZTX <948881549@qq.com>
2018-04-03 22:37:46 +0800

    重新上传之前内容 sharepreference记忆语种 实现语种切换 增加登录界面

--- app/src/main/java/com/bnuz/ztx/translateapp/Ui/SettingActivity.java
+++ app/src/main/java/com/bnuz/ztx/translateapp/Ui/SettingActivity.java
@@ -1,7 +1,6 @@
 package com.bnuz.ztx.translateapp.Ui;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
