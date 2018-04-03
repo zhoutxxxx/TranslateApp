094176737e0e883b8c3521e45cb9a6a68966b8ee
ZTX <948881549@qq.com>
2018-04-03 15:15:09 +0800

    增加语种切换功能（未实现选项记忆）

--- app/src/main/java/com/bnuz/ztx/translateapp/Ui/SettingActivity.java
+++ app/src/main/java/com/bnuz/ztx/translateapp/Ui/SettingActivity.java
@@ -1,7 +1,14 @@
 package com.bnuz.ztx.translateapp.Ui;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import com.bnuz.ztx.translateapp.R;
 
@@ -9,13 +16,16 @@ import org.angmarch.views.NiceSpinner;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by ZTX on 2018/4/2.
  */
 
-public class SettingActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
     NiceSpinner niceSpinner;
+    List<String> data;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +35,44 @@ public class SettingActivity extends AppCompatActivity {
 
     private void initView() {
         niceSpinner = (NiceSpinner) findViewById(R.id.exchangLanguage_np);
-        List<String> data = new LinkedList<>();
+        data = new LinkedList<>();
         data.add(getResources().getString(R.string.Exchange_language_Chinese_item));
         data.add(getResources().getString(R.string.Exchange_language_English_item));
+        data.add(getResources().getString(R.string.Exchange_language_Chinese_item));
         niceSpinner.attachDataSource(data);
+        niceSpinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l){
+        Resources resources = SettingActivity.this.getResources();
+        DisplayMetrics dm = resources.getDisplayMetrics();
+        Configuration config = resources.getConfiguration();
+        // 应用用户选择语言
+
+        switch (i){
+            case 0:
+                config.locale = Locale.SIMPLIFIED_CHINESE;
+                break;
+            case 1:
+                config.locale = Locale.ENGLISH;
+                break;
+            case 2:
+                config.locale = Locale.SIMPLIFIED_CHINESE;
+                break;
+            default:
+                config.locale = Locale.SIMPLIFIED_CHINESE;
+                break;
+        }
+        resources.updateConfiguration(config, dm);
+        Toast.makeText(getApplicationContext(),"成功修改语言为：" + data.get(i).toString(),Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, TabLayoutViewPager_Activity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        Toast.makeText(SettingActivity.this,"NiceSpinner index is null" , Toast.LENGTH_SHORT).show();
     }
 }
