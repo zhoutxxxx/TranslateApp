ebe98ac4b54fed5bc42e9a035f191db4b34212c5
ZTX <948881549@qq.com>
2018-04-19 08:21:28 +0800

    一。修改bug 1 无法翻译句子 2 翻译单词后，在翻译句子，listView的显示没有清空 3 语种切换，图标没有改变的bug 二。添加注释 三。简单的图像处理，图像识别的api接口设置

--- app/src/main/java/com/bnuz/ztx/translateapp/Fragment/TranslateFragment.java
+++ app/src/main/java/com/bnuz/ztx/translateapp/Fragment/TranslateFragment.java
@@ -39,6 +39,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -52,13 +53,14 @@ import java.util.List;
  */
 
 public class TranslateFragment extends Fragment implements View.OnClickListener {
-    TextView enter,microphone,photo,exchange,queryTv,phoneticTv;
-    NiceSpinner niceSpinner1,niceSpinner2;
+    TextView enter, microphone, photo, exchange, queryTv, phoneticTv;
+    NiceSpinner niceSpinner1, niceSpinner2;
     EditText input;
-    String url;
+    String url, OCRUrl;
     Uri imageUri;
     ListView informationListView;
     List<TranslateInformation> mList = new ArrayList<>();
+    List<String> explainsLists = new ArrayList<>();
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_translate, null);
@@ -66,6 +68,7 @@ public class TranslateFragment extends Fragment implements View.OnClickListener 
         findView(view);
         return view;
     }
+
     //实例化数据
     private void findView(View view) {
         //左下拉列表
@@ -87,45 +90,52 @@ public class TranslateFragment extends Fragment implements View.OnClickListener 
 //        dataset2.add(getResources().getString(R.string.Korean_language));
         niceSpinner2.attachDataSource(dataset2);
         //顶部交换按钮
-        exchange = (TextView)view.findViewById(R.id.exchange_tv);
+        exchange = (TextView) view.findViewById(R.id.exchange_tv);
         exchange.setText(getResources().getString(R.string.exchange_Icon));
         exchange.setTypeface(new FontManager().getType(getActivity()));
         //拍照按钮
-        photo = (TextView)view.findViewById(R.id.photo_tv);
+        photo = (TextView) view.findViewById(R.id.photo_tv);
         photo.setText(getResources().getString(R.string.camera_Icon));
         photo.setTypeface(new FontManager().getType(getActivity()));
         photo.setOnClickListener(this);
         //麦克风按钮
-        microphone = (TextView)view.findViewById(R.id.voice_tv);
+        microphone = (TextView) view.findViewById(R.id.voice_tv);
         microphone.setText(getResources().getString(R.string.microphone_Icon));
         microphone.setTypeface(new FontManager().getType(getActivity()));
         //翻译按钮
-        enter = (TextView)view.findViewById(R.id.enter_tv);
+        enter = (TextView) view.findViewById(R.id.enter_tv);
         enter.setText(getResources().getString(R.string.enter_Icon));
-        enter .setTypeface(new FontManager().getType(getActivity()));
+        enter.setTypeface(new FontManager().getType(getActivity()));
         enter.setOnClickListener(this);
         //输入框
-        input = (EditText)view.findViewById(R.id.input_et);
+        input = (EditText) view.findViewById(R.id.input_et);
         //Json数据显示框
-        informationListView = (ListView)view.findViewById(R.id.myListView);
-        phoneticTv = (TextView)view.findViewById(R.id.phonetic_tv);
-        queryTv = (TextView)view.findViewById(R.id.query_tv);
+        informationListView = (ListView) view.findViewById(R.id.myListView);
+        phoneticTv = (TextView) view.findViewById(R.id.phonetic_tv);
+        queryTv = (TextView) view.findViewById(R.id.query_tv);
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             //翻译按钮
             case R.id.enter_tv:
+                //获取到输入的文本
                 String s = input.getText().toString();
+                Logger.d("this EditText getText().toString():---->" + s);
+                //获取翻译状态，由什么语言翻译到什么语言
                 int fromInt = niceSpinner1.getSelectedIndex();
                 int toInt = niceSpinner2.getSelectedIndex();
                 try {
-                    url = new URLUtil().getTranslateURL(s,fromInt,toInt);
+                    //将数据传入，返回生成的url
+                    url = new URLUtil().getTranslateURL(s, fromInt, toInt).toString();
+                    //将带有空格的url转化成Android可以识别的url才可进行访问获取接送数据
+                    url = url.replace(" ", "%20");
                     Logger.t("ztx").d("url-------->" + url);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+                //网络请求接送数据
                 RxVolley.get(url, new HttpCallback() {
                     @Override
                     public void onSuccess(String t) {
@@ -133,7 +143,7 @@ public class TranslateFragment extends Fragment implements View.OnClickListener 
                     }
                 });
                 break;
-                //照相按钮
+            //照相按钮
             case R.id.photo_tv:
                 final String[] items = {getResources().getString(R.string.Take_photo)
                         , getResources().getString(R.string.Choose_from_the_album)
@@ -145,7 +155,7 @@ public class TranslateFragment extends Fragment implements View.OnClickListener 
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
                         //拍照
-                        if (which==0){
+                        if (which == 0) {
                             //调用摄像头
                             File outputImage = new File(getContext().getExternalCacheDir(), "output_image.jpg");
                             try {
@@ -170,10 +180,10 @@ public class TranslateFragment extends Fragment implements View.OnClickListener 
                             startActivityForResult(intent, 1);
                         }
                         //从相册中选择
-                        if(which==1){
-                                Intent intent = new Intent(Intent.ACTION_PICK);
-                                intent.setType("image/*");//相片类型
-                                startActivityForResult(intent, 2);
+                        if (which == 1) {
+                            Intent intent = new Intent(Intent.ACTION_PICK);
+                            intent.setType("image/*");//相片类型
+                            startActivityForResult(intent, 2);
                         }
                         Toast.makeText(getActivity(), items[which],
                                 Toast.LENGTH_SHORT).show();
@@ -183,36 +193,48 @@ public class TranslateFragment extends Fragment implements View.OnClickListener 
                 break;
         }
     }
+
     //事件处理
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
             //1 即拍照， 2 即从相册中选择
             case 1:
-              if(resultCode ==getActivity().RESULT_OK){
-                  try{
-                      Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver()
-                      .openInputStream(imageUri));
-                      ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                      bitmap.compress(Bitmap.CompressFormat.JPEG,100,baos);
-                      byte[] appicon = baos.toByteArray();
-                      String img_string = Base64.encodeToString(appicon,Base64.DEFAULT);
-                  }catch (FileNotFoundException e){
-                      e.printStackTrace();
-                  }
-              }
-                    break;
-            case 2:
-                if(resultCode ==getActivity().RESULT_OK){
-                   if(Build.VERSION.SDK_INT >=19){
-                       handleImageOnKitKat(data);
-                   }else{
-                       handleImageBeforeKitKat(data);
-                   }
+                if (resultCode == getActivity().RESULT_OK) {
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver()
+                                .openInputStream(imageUri));
+                        bitmap = comp(bitmap);
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 1, baos);
+                        byte[] appicon = baos.toByteArray();
+                        String imgString = Base64.encodeToString(appicon, Base64.DEFAULT);
+                        Logger.d(imgString.length());
+                        Logger.d("imgString   is   ------ >" + imgString);
+                        OCRUrl = new URLUtil().getOCRURL(imgString);
+                        Logger.d("OCRUrl   is   >>>>>>>>>>" + OCRUrl);
+//                      RxVolley.get(OCRUrl, new HttpCallback() {
+//                          @Override
+//                          public void onSuccess(String t) {
+//                              Logger.d(t);
+//                          }
+//                      });
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
                 }
                 break;
-              default:
-                    break;
+            case 2:
+                if (resultCode == getActivity().RESULT_OK) {
+                    if (Build.VERSION.SDK_INT >= 19) {
+                        handleImageOnKitKat(data);
+                    } else {
+                        handleImageBeforeKitKat(data);
+                    }
+                }
+                break;
+            default:
+                break;
         }
     }
 
@@ -221,6 +243,7 @@ public class TranslateFragment extends Fragment implements View.OnClickListener 
         String imagePath = getImagePath(uri, null);
         displayImage(imagePath);
     }
+
     /**
      * 4.4及以上的系统使用这个方法处理图片
      * API>=19
@@ -272,74 +295,142 @@ public class TranslateFragment extends Fragment implements View.OnClickListener 
         if (imagePath != null) {
             Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.JPEG,100,baos);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
             byte[] appicon = baos.toByteArray();
-            String img_string = Base64.encodeToString(appicon,Base64.DEFAULT);
+            String img_string = Base64.encodeToString(appicon, Base64.DEFAULT);
         } else {
             Toast.makeText(getActivity(), "failed to get image", Toast.LENGTH_SHORT).show();
         }
     }
 
+    private Bitmap compressImage(Bitmap image) {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);//质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
+        int options = 100;
+        while (baos.toByteArray().length / 1024 > 100) { //循环判断如果压缩后图片是否大于100kb,大于继续压缩
+            baos.reset();//重置baos即清空baos
+            image.compress(Bitmap.CompressFormat.JPEG, options, baos);//这里压缩options%，把压缩后的数据存放到baos中
+            options -= 10;//每次都减少10
+        }
+        ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());//把压缩后的数据baos存放到ByteArrayInputStream中
+        Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);//把ByteArrayInputStream数据生成图片
+        return bitmap;
+    }
+
+    private Bitmap comp(Bitmap image) {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        if (baos.toByteArray().length / 1024 > 1024) {//判断如果图片大于1M,进行压缩避免在生成图片（BitmapFactory.decodeStream）时溢出
+            baos.reset();//重置baos即清空baos
+            image.compress(Bitmap.CompressFormat.JPEG, 50, baos);//这里压缩50%，把压缩后的数据存放到baos中
+        }
+        ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());
+        BitmapFactory.Options newOpts = new BitmapFactory.Options();
+        //开始读入图片，此时把options.inJustDecodeBounds 设回true了
+        newOpts.inJustDecodeBounds = true;
+        Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, newOpts);
+        newOpts.inJustDecodeBounds = false;
+        int w = newOpts.outWidth;
+        int h = newOpts.outHeight;
+        //现在主流手机比较多是800*480分辨率，所以高和宽我们设置为
+        float hh = 800f;//这里设置高度为800f
+        float ww = 480f;//这里设置宽度为480f
+        //缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
+        int be = 1;//be=1表示不缩放
+        if (w > h && w > ww) {//如果宽度大的话根据宽度固定大小缩放
+            be = (int) (newOpts.outWidth / ww);
+        } else if (w < h && h > hh) {//如果高度高的话根据宽度固定大小缩放
+            be = (int) (newOpts.outHeight / hh);
+        }
+        if (be <= 0)
+            be = 1;
+        newOpts.inSampleSize = be;//设置缩放比例
+        //重新读入图片，注意此时已经把options.inJustDecodeBounds 设回false了
+        isBm = new ByteArrayInputStream(baos.toByteArray());
+        bitmap = BitmapFactory.decodeStream(isBm, null, newOpts);
+        return compressImage(bitmap);//压缩好比例大小后再进行质量压缩
+    }
 
 
-//解析Json
+    //解析Json
     private void parsingJson(String t) {
         try {
             Logger.t("ztx").json(t);
             JSONObject jsonObject = new JSONObject(t);
-            JSONObject json = jsonObject.getJSONObject("basic");
-            JSONArray explainsList = json.getJSONArray("explains");
-            TranslateInformation translateInformation = new TranslateInformation();
-            List<String> explainsLists = new ArrayList<>();
+            //数据的初始化，每次点击先清除之前查询的数据
+            explainsLists.clear();
             mList.clear();
-            //解析JSon数组的数据
-            for (int i = 0 ; i < explainsList.length(); i++){
-                String changeString = explainsList.get(i).toString();
-                StringBuffer sb = new StringBuffer();
-                //符号开关，只选取第一个.
-                boolean SYMBOL = false;
-                for (int k = 0 ; k < changeString.length() ; k++){
-                    if (changeString.charAt(k) == '.'){
-                        SYMBOL = true;
+            //实例化一个翻译对象
+            TranslateInformation translateInformation = new TranslateInformation();
+            //查询的文本
+            String query = jsonObject.getString("query");
+            translateInformation.setQuery(query);
+            queryTv.setText(translateInformation.getQuery());
+            //翻译的文本
+            String translations = jsonObject.getJSONArray("translation").get(0).toString();
+            translateInformation.setTranslations(translations);
+            phoneticTv.setText(translateInformation.getTranslations());
+            //翻译的详细内容
+            try {
+                JSONObject json = jsonObject.getJSONObject("basic");
+                JSONArray explainsList = json.getJSONArray("explains");
+                for (int i = 0; i < explainsList.length(); i++) {
+                    String changeString = explainsList.get(i).toString();
+                    StringBuffer sb = new StringBuffer();
+                    //符号开关，只选取第一个.
+                    boolean SYMBOL = false;
+                    for (int k = 0; k < changeString.length(); k++) {
+                        if (changeString.charAt(k) == '.') {
+                            SYMBOL = true;
+                        }
+                    }
+                    //搜索第一个.的位置
+                    int index = -1;
+                    if (SYMBOL) {
+                        //在android中添加html语言，改变风格
+                        sb.append(changeString).insert(0, "<font color='#FF0000'><i>");
+                        changeString = sb.toString();
+                    }
+                    for (int j = 0; j < changeString.length(); j++) {
+                        if (SYMBOL && changeString.charAt(j) == '.') {
+                            SYMBOL = false;
+                            index = j + 1;
+                        }
+                    }
+                    if (index != -1) {
+                        sb.insert(index, "</i></font> ");
+                        changeString = sb.toString();
+                    }
+                    explainsLists.add(changeString);
+                }
+                //英语音标的判定条件
+                if (niceSpinner1.getSelectedIndex() == 0) {
+                    try {
+                        String us_phonetic = json.getString("us-phonetic");
+                        String uk_phonetic = json.getString("uk-phonetic");
+                        String phonetic = json.getString("phonetic");
+                        translateInformation.setPhonetic(phonetic);
+                        translateInformation.setUk_phonetic(uk_phonetic);
+                        translateInformation.setUs_phonetic(us_phonetic);
+                        phoneticTv.setText(translateInformation.getENPhonetictoString());
+                    } catch (Exception e) {
+                        Logger.d("无json对象，因此不显示");
+                    }
+                } else if (niceSpinner2.getSelectedIndex() == 1) {//汉语拼音的判定条件
+                    try {
+                        String phonetic = json.getString("phonetic");
+                        translateInformation.setPhonetic(phonetic);
+                        phoneticTv.setText(translateInformation.getPhonetictoString());
+                    } catch (Exception e) {
+                        Logger.d("无json对象，因此不显示");
                     }
                 }
-                //搜索第一个.的位置，并将词性颜色标红
-                int index = -1;
-                if (SYMBOL){
-                    sb.append(changeString).insert(0,"<font color='#FF0000'><i>");
-                    changeString = sb.toString();
-                }
-                for (int j = 0 ; j < changeString.length() ; j++){
-                    if(SYMBOL && changeString.charAt(j) == '.'){
-                        SYMBOL = false;
-                        index = j + 1;
-                    }
-                }
-                if (index != -1){
-                    sb.insert(index,"</i></font> ");
-                    changeString = sb.toString();
-                }
-                explainsLists.add(changeString);
+            } catch (Exception e) {
+                Logger.d("无json对象，因此不显示");
             }
-            if (niceSpinner1.getSelectedIndex() == 0){
-                String query = jsonObject.getString("query");
-                String us_phonetic = json.getString("us-phonetic");
-                String uk_phonetic = json.getString("uk-phonetic");
-                String phonetic = json.getString("phonetic");
-                translateInformation.setPhonetic(phonetic);
-                translateInformation.setUk_phonetic(uk_phonetic);
-                translateInformation.setUs_phonetic(us_phonetic);
-                translateInformation.setQuery(query);
-                queryTv.setText(translateInformation.getQuery());
-                phoneticTv.setText(translateInformation.getENPhonetictoString());
-            }else{
-                String query = jsonObject.getString("query");
-                String phonetic = json.getString("phonetic");
-                translateInformation.setPhonetic(phonetic);
-                translateInformation.setQuery(query);
-                queryTv.setText(translateInformation.getQuery());
-                phoneticTv.setText(translateInformation.getPhonetictoString());
-            }
+            //添加ListView的适配器，是得到的文本都显示出来
             translateInformation.setExplains(explainsLists);
             mList.add(translateInformation);
             TranslateInformationAdapter adapter = new TranslateInformationAdapter(getActivity(), explainsLists);
--- app/src/main/java/com/bnuz/ztx/translateapp/Util/URLUtil.java
+++ app/src/main/java/com/bnuz/ztx/translateapp/Util/URLUtil.java
@@ -1,5 +1,10 @@
 package com.bnuz.ztx.translateapp.Util;
 
+import android.util.Base64;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.security.MessageDigest;
@@ -11,15 +16,27 @@ import java.util.Map;
  */
 
 public class URLUtil {
-        final String appKey = "073c24ee75a4bc9e";
-        final String appPassWord = "oCLsRlxIMRAoq5aRTxsDgiZWkgFsRZDs";
-        String query = null;
-        String salt = String.valueOf(System.currentTimeMillis());
-        String from = null;
-        String to = null;
-        String sign = null;
-    public String getTranslateURL(String query , int fromInt ,int toInt) throws Exception {
-        switch (fromInt){
+    final String appKey = "073c24ee75a4bc9e";
+    final String appPassWord = "oCLsRlxIMRAoq5aRTxsDgiZWkgFsRZDs";
+    String query = null;
+    String salt = String.valueOf(System.currentTimeMillis());
+    String from = null;
+    String to = null;
+    String sign = null;
+
+    final String OCRUrl = "http://openapi.youdao.com/ocrapi?";
+    final String OCRAppKey = "46f98245ce756749";
+    final String OCRAppPassWord = "oCLsRlxIMRAoq5aRTxsDgiZWkgFsRZDs";
+    String OCRDetectType = "10012";
+    String OCRImageType = "1";
+    String OCRLangType = "en";
+    String OCRDocType = "json";
+    String OCRSalt = String.valueOf(System.currentTimeMillis());
+    String OCRImg = null;
+    String OCRSign = null;
+
+    public String getTranslateURL(String query, int fromInt, int toInt) throws Exception {
+        switch (fromInt) {
             case 0:
                 this.from = "EN";
                 break;
@@ -27,7 +44,7 @@ public class URLUtil {
                 this.from = "zh-CHS";
                 break;
         }
-        switch (toInt){
+        switch (toInt) {
             case 0:
                 this.to = "zh-CHS";
                 break;
@@ -36,23 +53,24 @@ public class URLUtil {
                 break;
         }
         this.query = query;
-        sign = md5(appKey + query + salt+ appPassWord);
-        return "https://openapi.youdao.com/api?q="+ query +"&from=" + from + "&to=" + to + "&appKey=" + appKey + "&salt=" + salt + "&sign=" + sign;
+        sign = md5(appKey + query + salt + appPassWord);
+        return "https://openapi.youdao.com/api?q=" + query + "&from=" + from + "&to=" + to + "&appKey=" + appKey + "&salt=" + salt + "&sign=" + sign;
     }
 
     /**
      * 生成32位MD5摘要
+     *
      * @param string
      * @return
      */
     public String md5(String string) {
-        if(string == null){
+        if (string == null) {
             return null;
         }
         char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                 'A', 'B', 'C', 'D', 'E', 'F'};
 
-        try{
+        try {
             byte[] btInput = string.getBytes("utf-8");
             /** 获得MD5摘要算法的 MessageDigest 对象 */
             MessageDigest mdInst = MessageDigest.getInstance("MD5");
@@ -69,13 +87,14 @@ public class URLUtil {
                 str[k++] = hexDigits[byte0 & 0xf];
             }
             return new String(str);
-        }catch(NoSuchAlgorithmException | UnsupportedEncodingException e){
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
             return null;
         }
     }
 
     /**
      * 根据api地址和参数生成请求URL
+     *
      * @param url
      * @param params
      * @return
@@ -112,8 +131,11 @@ public class URLUtil {
 
         return builder.toString();
     }
+
+
     /**
      * 进行URL编码
+     *
      * @param input
      * @return
      */
@@ -129,5 +151,33 @@ public class URLUtil {
         }
 
         return input;
+    }
+
+    /**
+     * 获得图片的Base64编码
+     *
+     * @param imgFile
+     * @return
+     */
+    public String getImageStr(String imgFile) {//将图片文件转化为字节数组字符串，并对其进行Base64编码处理
+        InputStream in = null;
+        byte[] data = null;
+        //读取图片字节数组
+        try {
+            in = new FileInputStream(imgFile);
+            data = new byte[in.available()];
+            in.read(data);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //对字节数组Base64编码
+        return Base64.encodeToString(data,0);//返回Base64编码过的字节数组字符串
+    }
+
+    public String getOCRURL(String imageString) {
+        OCRImg = getImageStr(imageString);
+        OCRSign = md5(OCRAppKey + OCRImg + OCRSalt + OCRAppPassWord);
+        return OCRUrl + "appKey=" + OCRAppKey + "&img=" + OCRImg + "&detectType=" + OCRDetectType + "&imageType=" + OCRImageType + "&langType=" + OCRLangType + "&salt=" + OCRSalt + "&docType=" + OCRDocType + "&sign=" + OCRSign;
     }
 }
